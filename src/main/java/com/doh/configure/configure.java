d85bbe673b27cc4a97fdac6edf3b5389e46c18e2
DohoonHan <hsm3809@naver.com>
2024-09-02 11:42:01 +0900

    Initial commit

---
+++ src/main/java/com/doh/configure/configure.java
@@ -0,0 +1,32 @@
+package com.doh.configure;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan("com.doh.mappers")
+public class configure {
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mappers/**/*.xml"));
+        return sessionFactory.getObject();
+    }
+    @Bean
+    public DataSource dataSource() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setJdbcUrl("jdbc:mysql://teamdb.cx6acq2ikt1l.ap-northeast-2.rds.amazonaws.com:3306/DOH");
+        dataSource.setUsername("abc3");
+        dataSource.setPassword("123456789");
+        return dataSource;
+    }
+}
---
+++ src/test/java/com/doh/doh/DohApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doh.doh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DohApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
